caabf7a034218f2803a8aa82842aaa451fa97815
timolawl <timolawl@gmail.com>
2017-03-06 19:59:06 -0800

    Update UI

--- app/client/scripts/post.js
+++ app/client/scripts/post.js
@@ -2,3 +2,13 @@
 
 // socket io -- the cdnjs script is in the HTML template above this script file
 var socket = io();
+
+window.onload = function () {
+  document.querySelector('.bar__search').addEventListener('click', e => {
+    if (e.target.classList.contains('bar__search--submit')) {
+      console.log(document.querySelector('.bar__search--input').value);
+    }
+  });
+};
+
+
